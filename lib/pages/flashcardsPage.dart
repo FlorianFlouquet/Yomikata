a1ac3a5f82fa6077f36ece92e754e4431a140670
FlorianFlouquet <florian.flouquet2@hotmail.com>
2022-11-01 18:56:09 +0100

    feature: can flip the card and see both sides

--- lib/pages/flashcardsPage.dart
+++ lib/pages/flashcardsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
 
 class FlashCardsPage extends StatelessWidget {
   const FlashCardsPage({Key? key}) : super(key: key);
@@ -21,74 +22,91 @@ class FlashCardsPage extends StatelessWidget {
         ),
         body: Column(
           children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: Container(
-                height: 500,
-                padding: EdgeInsets.all(20),
-                child: Center(
-                  child: Stack(
-                    children: [
-                      Transform(
-                        transform: Matrix4.rotationZ(3),
-                        alignment: FractionalOffset.center,
-                        child: Container(
-                          width: 250,
-                          height: 400,
-                          child: Card(
-                            color: Colors.grey[300],
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                color: Colors.black,
-                                width: 1,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Transform(
-                        transform: Matrix4.rotationZ(0.2),
-                        alignment: FractionalOffset.center,
-                        child: Container(
-                          width: 250,
-                          height: 400,
-                          child: Card(
-                            color: Colors.grey[300],
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                color: Colors.black,
-                                width: 1,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
+            Container(
+              height: 500,
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Stack(
+                  children: [
+                    Transform(
+                      transform: Matrix4.rotationZ(3),
+                      alignment: FractionalOffset.center,
+                      child: Container(
                         width: 250,
                         height: 400,
                         child: Card(
                           color: Colors.grey[300],
                           shape: RoundedRectangleBorder(
-                              side: BorderSide(
-                                color: Colors.black,
-                                width: 1,
-                              )
+                            side: BorderSide(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Transform(
+                      transform: Matrix4.rotationZ(0.2),
+                      alignment: FractionalOffset.center,
+                      child: Container(
+                        width: 250,
+                        height: 400,
+                        child: Card(
+                          color: Colors.grey[300],
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    FlipCard(
+                      fill: Fill.fillBack,
+                      direction: FlipDirection.HORIZONTAL,
+                      front: Container(
+                        width: 250,
+                        height: 400,
+                        child: Card(
+                          color: Colors.grey[300],
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.black,
+                              width: 1,
+                            ),
                           ),
                           child: Center(
                             child: Text(
                               "漢字",
                               style: TextStyle(
-                                fontSize: 32,
                                 fontWeight: FontWeight.bold,
+                                fontSize: 32,
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                      back: Card(
+                        color: Colors.grey[300],
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "かんじ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 32,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -100,7 +118,7 @@ class FlashCardsPage extends StatelessWidget {
                   fontSize: 16,
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
