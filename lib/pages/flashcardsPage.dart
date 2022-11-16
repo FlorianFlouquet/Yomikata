f17065b86c87ea77eb4c0cf99c9d38a7519f52a3
FlorianFlouquet <florian.flouquet2@hotmail.com>
2022-11-16 15:14:26 +0100

    feat: can now navigate between Home, Decks and Add pages

--- lib/pages/flashcardsPage.dart
+++ lib/pages/flashcardsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
+import 'package:yomikata/components/customAppBar.dart';
 import 'package:yomikata/types/deckType.dart';
 import '../types/cardType.dart';
 
@@ -56,109 +57,32 @@ class _FlashCardsPageState extends State<FlashCardsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 70,
-          title: Container(
-            padding: const EdgeInsets.all(12),
-            alignment: AlignmentDirectional.centerStart,
-            child: const Icon(
-              Icons.menu,
-              size: 40,
-            ),
-          ),
-          backgroundColor: Color(0xff670D0D),
-        ),
-        body: !gameEnded ?
-          OverflowBox(
-            minWidth: 0.0,
-            maxWidth: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                DragTarget(
-                    onAccept: (data) => wrong(),
-                    builder: (context, _, __) => Container(
-                      height: 450,
-                      width: 270,
-                    )
-                ),
-                Column(
-                  children: [
-                    Container(
-                      height: 600,
-                      padding: EdgeInsets.all(20),
-                      child: Center(
-                        child: Stack(
-                          children: [
-                            Transform(
-                              transform: Matrix4.rotationZ(3),
-                              alignment: FractionalOffset.center,
-                              child: Container(
-                                width: 270,
-                                height: 450,
-                                child: Card(
-                                  color: Colors.grey[300],
-                                  shape: const RoundedRectangleBorder(
-                                    side: BorderSide(
-                                      color: Colors.black,
-                                      width: 1,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Transform(
-                              transform: Matrix4.rotationZ(0.2),
-                              alignment: FractionalOffset.center,
-                              child: Container(
-                                width: 270,
-                                height: 450,
-                                child: Card(
-                                  color: Colors.grey[300],
-                                  shape: const RoundedRectangleBorder(
-                                    side: BorderSide(
-                                      color: Colors.black,
-                                      width: 1,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Draggable(
-                              data: "OUI",
-                              feedback: Container(
-                                width: 270,
-                                height: 450,
-                                child: Card(
-                                  color: Colors.grey[300],
-                                  shape: const RoundedRectangleBorder(
-                                    side: BorderSide(
-                                      color: Colors.orange,
-                                      width: 1,
-                                    ),
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      _card.kanji,
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 32,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              childWhenDragging: Container(
-                                width: 270,
-                                height: 400,
-                                color: const Color.fromRGBO(0, 0, 0, 0),
-                              ),
-                              child: FlipCard(
-                                fill: Fill.fillBack,
-                                direction: FlipDirection.HORIZONTAL,
-                                front: Container(
+    return !gameEnded
+            ? OverflowBox(
+              minWidth: 0.0,
+              maxWidth: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DragTarget(
+                      onAccept: (data) => wrong(),
+                      builder: (context, _, __) => Container(
+                        height: 450,
+                        width: 270,
+                      )
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 600,
+                        padding: EdgeInsets.all(20),
+                        child: Center(
+                          child: Stack(
+                            children: [
+                              Transform(
+                                transform: Matrix4.rotationZ(3),
+                                alignment: FractionalOffset.center,
+                                child: Container(
                                   width: 270,
                                   height: 450,
                                   child: Card(
@@ -166,6 +90,39 @@ class _FlashCardsPageState extends State<FlashCardsPage> {
                                     shape: const RoundedRectangleBorder(
                                       side: BorderSide(
                                         color: Colors.black,
+                                        width: 1,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Transform(
+                                transform: Matrix4.rotationZ(0.2),
+                                alignment: FractionalOffset.center,
+                                child: Container(
+                                  width: 270,
+                                  height: 450,
+                                  child: Card(
+                                    color: Colors.grey[300],
+                                    shape: const RoundedRectangleBorder(
+                                      side: BorderSide(
+                                        color: Colors.black,
+                                        width: 1,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Draggable(
+                                data: "OUI",
+                                feedback: Container(
+                                  width: 270,
+                                  height: 450,
+                                  child: Card(
+                                    color: Colors.grey[300],
+                                    shape: const RoundedRectangleBorder(
+                                      side: BorderSide(
+                                        color: Colors.orange,
                                         width: 1,
                                       ),
                                     ),
@@ -180,102 +137,130 @@ class _FlashCardsPageState extends State<FlashCardsPage> {
                                     ),
                                   ),
                                 ),
-                                back: Card(
-                                  color: Colors.grey[300],
-                                  shape: const RoundedRectangleBorder(
-                                    side: BorderSide(
-                                      color: Colors.black,
-                                      width: 1,
+                                childWhenDragging: Container(
+                                  width: 270,
+                                  height: 400,
+                                  color: const Color.fromRGBO(0, 0, 0, 0),
+                                ),
+                                child: FlipCard(
+                                  fill: Fill.fillBack,
+                                  direction: FlipDirection.HORIZONTAL,
+                                  front: Container(
+                                    width: 270,
+                                    height: 450,
+                                    child: Card(
+                                      color: Colors.grey[300],
+                                      shape: const RoundedRectangleBorder(
+                                        side: BorderSide(
+                                          color: Colors.black,
+                                          width: 1,
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          _card.kanji,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 32,
+                                          ),
+                                        ),
+                                      ),
                                     ),
                                   ),
-                                  child: Center(
-                                    child: Text(
-                                      _card.pronun,
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 32,
+                                  back: Card(
+                                    color: Colors.grey[300],
+                                    shape: const RoundedRectangleBorder(
+                                      side: BorderSide(
+                                        color: Colors.black,
+                                        width: 1,
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        _card.pronun,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 32,
+                                        ),
                                       ),
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    const Center(
-                      child: Text(
-                        "click on the card to flip it",
-                        style: TextStyle(
-                          fontStyle: FontStyle.italic,
-                          fontSize: 16,
+                      const Center(
+                        child: Text(
+                          "click on the card to flip it",
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                DragTarget(
-                    onAcceptWithDetails: (data) => right(),
-                    builder: (context, _, __) => Container(
-                      height: 450,
-                      width: 270,
-                    )
-                ),
-              ],
-            ),
-          ) :
-          Center(
-            child: Container(
-              height: 400,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(30),
-                    height: 100,
-                    width: 200,
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Color(0xff670D0D),
-                      ),
-                      child: Text(
-                        'Homepage',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20
-                        ),
-                      ),
-                    ),
+                    ],
                   ),
-                  Container(
-                    margin: EdgeInsets.all(30),
-                    height: 100,
-                    width: 200,
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      style: TextButton.styleFrom(
-                          backgroundColor: Color(0xff670D0D)
-                      ),
-                      child: Text(
-                        'Text review',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20
-                        ),
-                      ),
-                    ),
-                  )
+                  DragTarget(
+                      onAcceptWithDetails: (data) => right(),
+                      builder: (context, _, __) => Container(
+                        height: 450,
+                        width: 270,
+                      )
+                  ),
                 ],
               ),
-            ),
-          )
-      ),
-    );
+            )
+            : Center(
+              child: Container(
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(30),
+                      height: 100,
+                      width: 200,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Color(0xff670D0D),
+                        ),
+                        child: Text(
+                          'Homepage',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(30),
+                      height: 100,
+                      width: 200,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: Color(0xff670D0D)
+                        ),
+                        child: Text(
+                          'Text review',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
   }
 }
--- lib/pages/gameConfigPage.dart
+++ lib/pages/gameConfigPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yomikata/components/customAppBar.dart';
+import 'package:yomikata/pages/addDeckPage.dart';
 import 'package:yomikata/service/deckService.dart';
-import '../mongoDb.dart';
 import 'flashcardsPage.dart';
 import '../types/deckType.dart';
 import '../components/deck.dart';
@@ -50,7 +51,6 @@ class GameConfigPage extends StatefulWidget {
 class _GameConfigPageState extends State<GameConfigPage> {
 
   late List<DeckType> _selectedDeck = [];
-
   late List<DeckType> decks = [];
 
   @override
@@ -88,97 +88,83 @@ class _GameConfigPageState extends State<GameConfigPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 70,
-          backgroundColor: Color(0xff670D0D),
-          title: Container(
-            padding: const EdgeInsets.all(12),
-            alignment: AlignmentDirectional.centerStart,
-            child: const Icon(
-              Icons.menu,
-              size: 40,
+    return ListView(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(32),
+          child: const Center(
+            child: Text(
+              'CHOOSE YOUR DECKS',
+              style: TextStyle(
+                color: Color(0xff670D0D),
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
-        body: ListView(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(32),
-              child: const Center(
-                child: Text(
-                  'CHOOSE YOUR DECKS',
-                  style: TextStyle(
-                    color: Color(0xff670D0D),
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+        decks.isEmpty ? Text("LOADING") : Container(
+          height: 170,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: decks.map((deck) {
+              return Deck(
+                deck: deck,
+                addDeck: addDeck,
+                removeDeck: removeDeck,
+              );
+            }).toList(),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(32),
+          child: const Center(
+            child: Text(
+              'CHOOSE A GAME MODE',
+              style: TextStyle(
+                color: Color(0xff670D0D),
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            decks.isEmpty ? Text("LOADING") : Container(
-              height: 170,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: decks.map((deck) {
-                  return Deck(
-                    deck: deck,
-                    addDeck: addDeck,
-                    removeDeck: removeDeck,
-                  );
-                }).toList(),
+          ),
+        ),
+        Container(
+          height: 170,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              GameMode(title: "FLASHCARDS"),
+              GameMode(title: "MCQ"),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(45),
+          child: TextButton(
+            onPressed: _selectedDeck.length == 0 ?
+            null :
+                () {
+              print(context);
+              Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (context) => FlashCardsPage(deck: _selectedDeck))
+              );
+            },
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: _selectedDeck.length == 0 ? Colors.grey : Color(0xff670D0D),
+              padding: EdgeInsets.fromLTRB(50, 30, 50, 30),
+            ),
+            child: Text(
+              "START",
+              style: TextStyle(
+                fontSize: 20,
               ),
             ),
-            Container(
-              padding: const EdgeInsets.all(32),
-              child: const Center(
-                child: Text(
-                  'CHOOSE A GAME MODE',
-                  style: TextStyle(
-                    color: Color(0xff670D0D),
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 170,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  GameMode(title: "FLASHCARDS"),
-                  GameMode(title: "MCQ"),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(45),
-              child: TextButton(
-                onPressed: _selectedDeck.length == 0 ?
-                  null :
-                  () {
-                    print(context);
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => FlashCardsPage(deck: _selectedDeck))
-                    );
-                },
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: _selectedDeck.length == 0 ? Colors.grey : Color(0xff670D0D),
-                  padding: EdgeInsets.fromLTRB(50, 30, 50, 30),
-                ),
-                child: Text(
-                  "START",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        )
+          ),
+        ),
+      ],
     );
   }
 }
