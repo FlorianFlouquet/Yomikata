79be5359d7f8bb9494390d115db6246035e26fa4
FlorianFlouquet <florian.flouquet2@hotmail.com>
2022-11-27 15:21:48 +0100

    feat: added an empty profile page for later

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,2 @@
-const MONGO_URL = "mongodb+srv://root:root@cluster0.fta1qxl.mongodb.net/test?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://root:root@cluster0.fta1qxl.mongodb.net/?retryWrites=true&w=majority";
 const COLLECTION_NAME = "users";
